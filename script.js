84b8e6d71682ab3ee801006472215f1d506c3228
meghanathul <92531334+meghanathul@users.noreply.github.com>
2022-03-27 21:09:20 +0530

    js7

---
+++ script.js
@@ -0,0 +1,34 @@
+// const a=[6,'Meghana',1.25]
+// console.log(a)
+
+// var arr=[1,2,3,4,5,6,7,8,9,10]
+// var result=arr.map((x)=>x*2
+// )
+// var result=arr.reduce((x,y)=>x=x+y,0)
+// console.log(result)
+
+
+
+// arr.push('6');
+// console.log(arr)  //add in end
+
+
+// arr.pop();
+// console.log(arr)  //remove element in end
+
+// arr.unshift(77);
+// console.log(arr)  //ad delement in starting
+
+// arr.shift();
+// console.log(arr)  //remove first element
+
+// const res=arr.some((x)=>x%2==0);
+// console.log(res)
+
+// const res=arr.slice(1,3);
+// console.log(res)
+
+// indexOf
+const arr=['Meghana']
+const res=arr[0][5]
+console.log(res)
